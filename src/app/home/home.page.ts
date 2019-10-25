c907ef8c5888470a6c2943cf52cedb7600423364
Jesus bossa <x@jesusbossa.com>
2019-10-25 15:19:59 -0500

    [ADD] Confirm songs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import songs from '../../DB/songs.json'
+import people from '../../DB/people.json'
 
 @Component({
   selector: 'app-home',
@@ -10,13 +11,18 @@ export class HomePage {
 
   private songlist: Array<object> = songs
   public selectedSong: object = {}
+  public confirmedSong: object = songs[0]
+  public peopleList: Array<object> = people
+  public peopleSelected: Array<object> = []
 
   constructor() {
-    console.log(this.songlist)
-    console.log(this.selectedSong)
+    console.log(this.peopleList)
+    console.log(this.peopleSelected)
+    // this.selectedSong['status'] = true
   }
 
   public randomSong() {
+    this.confirmedSong = {}
     const random = this.getRandomInt(0, this.songlist.length)
     this.selectedSong = this.songlist[random]
     console.log(this.selectedSong)
@@ -24,6 +30,11 @@ export class HomePage {
 
   private getRandomInt(min: number = 0, max: number = 5) {
     return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  public confirmSong() {
+    this.confirmedSong = this.selectedSong
+    this.selectedSong = {}
   }
 
 }
